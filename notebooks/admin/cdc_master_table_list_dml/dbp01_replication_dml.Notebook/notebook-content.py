b8d30667d561894dd5f1e24fd7f76bf18c91c6ec
Sreejith Munthikodu <152909343+smunthik@users.noreply.github.com>
2026-06-17 14:22:15 -0700

    Committing 5 items from workspace 7a798b59-e76b-4af6-9ffb-fa0a2959519c

--- notebooks/admin/cdc_master_table_list_dml/dbp01_replication_dml.Notebook/notebook-content.py
+++ notebooks/admin/cdc_master_table_list_dml/dbp01_replication_dml.Notebook/notebook-content.py
@@ -6,12 +6,23 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "c35a1880-4d21-42b2-b055-983b7ac0563b",
+# META       "default_lakehouse_name": "bcts_lakehouse",
+# META       "default_lakehouse_workspace_id": "7a798b59-e76b-4af6-9ffb-fa0a2959519c",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "c35a1880-4d21-42b2-b055-983b7ac0563b"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # MARKDOWN ********************
 
-# ## BCTSADMIN
+# ### BCTSADMIN
 
 
 # CELL ********************
